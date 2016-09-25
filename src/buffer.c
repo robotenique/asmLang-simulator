ee494e04e71917cdd82f76d70e103b0b75cbdf93
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-09-24 21:30:15 -0300

    Add functions signature to buffer.c

--- src/buffer.c
+++ src/buffer.c
@@ -6,3 +6,23 @@
  *
  * Buffer Implementation
  */
+
+ Buffer *buffer_create() {
+
+ }
+
+ void buffer_destroy(Buffer *B) {
+
+ }
+
+ void buffer_reset(Buffer *B){
+
+ }
+
+ void buffer_push_back(Buffer *B, char c){
+
+ }
+ 
+ int read_line(FILE *input, Buffer *B){
+
+ }
