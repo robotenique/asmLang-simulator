f208293c6273ed134a94cb8051a76106166d1434
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-09-27 17:06:10 -0300

    Add the complete buffer.c file, with the definition of all the functions necessary for the data structure

--- src/buffer.c
+++ src/buffer.c
@@ -6,23 +6,48 @@
  *
  * Buffer Implementation
  */
+#include "../include/buffer.h"
+#include <stdlib.h>
 
+/* REMAINDER: The 0 signals the end of a string. Remember to use the
+ * buffer_push_back() function to push the 0 when finished reading
+ * the proper string!
+ */
  Buffer *buffer_create() {
-
+     Buffer *B = malloc(sizeof(Buffer));
+     B -> n = 1024;
+     B -> i = 0;
+     B -> data = malloc(B -> n) ;
+     return B;
  }
 
  void buffer_destroy(Buffer *B) {
-
+     free(B -> data);
+     free(B);
  }
 
  void buffer_reset(Buffer *B){
-
+    free(B -> data);
+    B -> data = malloc(B -> n);
+    B -> i = 0;
  }
 
  void buffer_push_back(Buffer *B, char c){
-
+     B -> data[B->i++] = c;
  }
- 
- int read_line(FILE *input, Buffer *B){
 
+ int read_line(FILE *input, Buffer *B){
+    char c;
+    int count = 0;
+    buffer_reset(B);
+
+    while((c = fgetc(input)) != EOF && c != '\n') {
+        B -> data[B -> i++] = c;
+        count ++;
+    }
+    if (c == '\n') {
+        B -> data[B -> i++] = c;
+        count ++;
+    }
+    return count;
  }
