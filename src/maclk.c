9dae1557bfbfbaaaa07f94cf1e25483bf1f62a78
allanamancio <allanamancio.rocha@gmail.com>
2016-12-11 23:40:05 -0200

    Update maclk.c

--- src/maclk.c
+++ src/maclk.c
@@ -72,14 +72,17 @@ int main(int argc, char *argv[])
 
     set_prog_name("Linker");
 
+    /*Verifica se os arquivos estão corretos.*/
     if (!(inputs = openFiles(argc, argv))) {
         set_error_msg("Error: invalid argument(s).");
         die(NULL);
     }
 
+    /*Verifica se existem rótulos do EXTERN repetidos*/
     for (int i = 0, main = 0; i < argc - 1; i++) {
         while (read_line(inputs[i], line) && line->data[0] != 'B') {
             if (line->data[0] == 'E') {
+                /*Coloca o rótulo na tabela de símbolos com a posição da instrução.*/
                 wordlen = read_word(*line->data[2], word);
                 if (!strcmp(word->data, "main")) main = 1;
                 if (!(result = stable_insert(extern_table, word->data))) {
@@ -92,90 +95,115 @@ int main(int argc, char *argv[])
                     *(result.data) = num;
                 }
             }
-            else numinstr[i] = atoi(line->data);
+            else numinstr[i] = atoi(line->data); /*Número de instruções*/
         }
     }
 
+    /*Verifica se a main foi definida.*/
     if (!main) {
         set_error_msg("Error: main not defined.");
         die(NULL);
     }
 
+    /*Fecha os arquivos e os abre de novo.*/
     for (int i = 0; i < argc - 1; i++) fclose(inputs[i]);
     output = fopen(argv[0], "a+");
     inputs[0] = fopen(argv[1], "r");
     while (read_line(inputs[0], line) && line->data[0] != 'B');
 
-    data = stable_find(extern_table, "main");
-
+    /*Coloca a primeira instrução - JMP para a main.*/
+    data = stable_find(extern_table, "main") + 1;
     if (*data > 16777215) {
         set_error_msg("Error: JMP is very large.");
         die(NULL);
     }
     sprintf(jmp, "%06x\n", *data);
     fprintf(output, strcat("48", jmp));
-    numline = 1;
-    while (read_line(inputs[0], line)) {
-        numline++;
-        if (line->data[0] == '*') {
-            wordlen = read_word(*line->data[2], word);
-            opcode = what_jmp(word->data);
-            if (strcmp(opcode, "JMP")) {
-                read_word(*line->data[2+wordlen+1], word);
-                strcat(opcode, word->data);
-                read_word(*line->data[2+wordlen+1+3], word);
-            }
-            else read_word(*line->data[6], word);
-            jmp = 0;
-            for (int i = 1; i < argc - 1 && !jmp; i++) {
-                inputs[i] = fopen(argv[i+1], "r");
-                while (read_line(inputs[i], line) && line->data[0] != 'E');
-                do {
-                    read_word(line->data[2], wordtmp);
-                    if (!strcmp(word->data, wordtmp->data)) {
-                        numjmp = numinstr[0] - numline;
-                        for (int j = 0; j < i; numjmp += numinstr[j++]);
-                        data = stable_find(extern_table, word->data);
-                        numjmp += *data;
-                        if (strcmp(opcode, "JMP")) {
-                            if (numjmp > 65535) {
-                                set_error_msg("Error: JMP is very large.");
-                                die(NULL);
-                            }
-                            sprintf(numjmp, "%04x\n", numjmp);
-                            strcat(opcode, numjmp);
-                            opcode[strlen(opcode)-1] = '\n';
-                            fprintf(output, opcode);
-                        }
-                        else {
-                            if (numjmp > 16777215) {
-                                set_error_msg("Error: JMP is very large.");
-                                die(NULL);
-                            }
-                            sprintf(numjmp, "%06x\n", numjmp);
-                            strcat(opcode, numjmp);
-                            opcode[strlen(opcode)-1] = '\n';
-                            fprintf(output, opcode);
-                        }
-                        jmp = 1;
-                        break;
-                    }
-                } while (read_line(inputs[i], line) && line->data[0] != 'B');
-                fclose(inputs[i]);
-            }
-            if (!jmp) {
-                set_error_msg("Error: JMP not defined.");
-                die(NULL);
-            }
-        }
-        else fprintf(output, line->data);
-    }
-    for (int i = 1; i < argc - 1; i++) {
-        while (read_line(inputs[i], line)) {
-            fprintf(output, line->data);
-        }
-    }
 
+    /*Junta todos os códigos-objetos em um só e resolve as pendências.*/
+    for (int k = 0, numline = 1; k < argc - 1; k++) {
+        /*Em cada iteração nova, um código-objeto diferente.*/
+        while (read_line(inputs[k], line)) {
+            numline++; /*Nova linha.*/
+            if (line->data[0] == '*') {
+                /*Instrução pendente.*/
+
+                /*Construção da instrução sem o número de instruções a ser pulada.*/
+                wordlen = read_word(*line->data[2], word);
+                opcode = what_jmp(word->data);
+                if (strcmp(opcode, "JMP")) {
+                    /*JZ, JNZ, JP, JNN, ... (menos o JMP).*/
+                    read_word(*line->data[2+wordlen+1], word);
+                    strcat(opcode, word->data);
+                    read_word(*line->data[2+wordlen+1+3], word);
+                }
+                else read_word(*line->data[6], word);
+                
+                jmp = 0; /*Sinaliza se há um extern relacionado ao JMP pendente.*/
+                
+                /*Procura o rótulo para o qual o fluxo do programa desviará.*/
+                for (int i = 0; i < argc - 1 && !jmp; i++) {
+                    if (i == k) continue; /*Ignora o JMP pro próprio código-objeto.*/
+
+                    inputs[i] = fopen(argv[i+1], "r");
+                    while (read_line(inputs[i], line) && line->data[0] != 'E');
+                    
+                    /*Verifica se o rótulo está no código-objeto desta iteração.*/
+                    do {
+                        read_word(line->data[2], wordtmp);
+                        if (!strcmp(word->data, wordtmp->data)) {
+                            /*O rótulo foi encontrado.*/
+
+                            /*Cálculo do pulo.*/
+                            data = stable_find(extern_table, word->data);
+                            if (i > k) {
+                                numjmp = numinstr[0] - numline;
+                                for (int j = k + 1; j < i; numjmp += numinstr[j++]);
+                                numjmp += (*data + 1);
+                            }
+                            else {
+                                numjmp = numline - 1;
+                                for (int j = k - 1; j > i; numjmp += numinstr[j++]);
+                                numjmp += (numinstr[i] - *data);
+                            }
+
+                            /*Verifica se o pulo é possível.*/
+                            if (strcmp(opcode, "JMP")) {
+                                if (numjmp > 65535) {
+                                    set_error_msg("Error: JMP is very large.");
+                                    die(NULL);
+                                }
+                                sprintf(numjmp, "%04x\n", numjmp);
+                            }
+                            else {
+                                if (numjmp > 16777215) {
+                                    set_error_msg("Error: JMP is very large.");
+                                    die(NULL);
+                                }
+                                sprintf(numjmp, "%06x\n", numjmp);
+                            }
+
+                            /*Termina de construir a instrução.*/
+                            strcat(opcode, numjmp);
+                            opcode[strlen(opcode)-1] = '\n';
+                            if (i < k) opcode[1]++;
+                            fprintf(output, opcode);
+                            jmp = 1;
+                            break;
+                        }
+                    } while (read_line(inputs[i], line) && line->data[0] != 'B');
+                    fclose(inputs[i]);
+                }
+                if (!jmp) {
+                    set_error_msg("Error: JMP not defined.");
+                    die(NULL);
+                }
+            }
+            else fprintf(output, line->data);
+        }
+        numline = 0;
+    }
+    
     buffer_destroy(word);
     buffer_destroy(wordtmp);
     buffer_destroy(wordline);
